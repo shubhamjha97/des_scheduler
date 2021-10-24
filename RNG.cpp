668d0b160cf89f4314a95a0e96ac5d7af4c542b5
Shubham Jha <jha1shubham@gmail.com>
2021-10-23 23:49:49 -0400

    Fix bug in RNG. Read first line into n

--- RNG.cpp
+++ RNG.cpp
@@ -8,12 +8,16 @@ class RNG {
 
 private:
     vector<int> rnos;
-    int randSize, ofs;
+    int randSize, ofs, n;
 
     void readRandomNumberFile(string randomFilePath) {
         ifstream infile(randomFilePath);
         string line;
-        while (getline(infile, line)) {
+        getline(infile, line);
+        n = stoi(line);
+        // TODO: The first line represents the number of rnos
+        for(int i=0; i<n; i++) {
+            getline(infile, line);
             rnos.push_back(stoi(line));
         }
     }
