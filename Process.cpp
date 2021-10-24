d81121ded3e4a41f8d962118531c568a15d79a4d
Shubham Jha <jha1shubham@gmail.com>
2021-10-24 02:17:05 -0400

    Add additional fields to Process

--- Process.cpp
+++ Process.cpp
@@ -1,9 +1,22 @@
 #include "Process.h"
 
-Process::Process(int pid, int arrivalTime, int totalCpuTime, int cpuBurst, int ioBurst) {
+Process::Process(
+        int pid,
+        int arrivalTime,
+        int totalCpuTime,
+        int cpuBurst,
+        int ioBurst,
+        int staticPriority,
+        int dynamicPriority) {
     this -> pid = pid;
     this -> arrivalTime = arrivalTime;
     this -> totalCpuTime = totalCpuTime;
     this -> cpuBurst = cpuBurst;
     this -> ioBurst = ioBurst;
+    this -> staticPriority = staticPriority;
+    this -> dynamicPriority = dynamicPriority;
+    this -> cpuWaitTime = 0;
+    this -> finishTime = 0;
+    this -> turnaroundTime = 0;
+    this -> ioTime = 0;
 }
--- Process.h
+++ Process.h
@@ -5,8 +5,10 @@ class Process {
 public:
     int arrivalTime, totalCpuTime, cpuBurst, ioBurst;
     int pid;
+    int staticPriority, dynamicPriority; // TODO: initialize priority values
+    int cpuWaitTime, finishTime, turnaroundTime, ioTime;
 
-    Process(int pid, int arrivalTime, int totalCpuTime, int cpuBurst, int ioBurst);
+    Process(int pid, int arrivalTime, int totalCpuTime, int cpuBurst, int ioBurst, int staticPriority, int dynamicPriority);
 };
 
 #endif //SCHEDULER_PROCESS_H
