d0b0f30a6fb719120362622e1f2939e362f0fb4b
Shubham Jha <jha1shubham@gmail.com>
2021-10-24 02:16:37 -0400

    Add method to dump results to STDOUT

--- helper.cpp
+++ helper.cpp
@@ -4,14 +4,16 @@
 #include <iostream>
 #include <sstream>
 #include "Process.h"
+#include "RNG.h"
 
 using namespace std;
 
-static vector<Process*> readProcessFile(string &processFilePath) {
+static vector<Process*> readProcessFile(string &processFilePath, int maxPriority, RNG *rng) {
     vector<Process*> processes;
     ifstream infile(processFilePath);
     string line;
     int currentPid = 0;
+    int MAX_PRIO = maxPriority;
 
     while(getline(infile, line)){   // read a line
         vector<int> processParams;
@@ -23,17 +25,43 @@ static vector<Process*> readProcessFile(string &processFilePath) {
         if(processParams.size() != 4) {
             break;
         }
+
+        int staticPriority = rng -> random(MAX_PRIO); // TODO: ensure random numbers are generated in the correct range
+        int dynamicPriority = staticPriority - 1; // TODO: Check if this is correct
+
         processes.push_back(
                 new Process(
                         currentPid, // TODO: Check whether pids are being allocated and incremented correctly
                         processParams[0],
                         processParams[1],
                         processParams[2],
-                        processParams[3]
+                        processParams[3],
+                        staticPriority,
+                        dynamicPriority
                 )
         );
         currentPid++;
     }
 
     return processes;
+}
+
+void dumpResultsToConsole(string schedulerName, int timeQuantum, const vector<Process*>& processes) {
+    // TODO: Add scheduler info
+    // TODO: use correct number of decimal places
+    for(auto process: processes) {
+        printf(
+                "%d: %d %d %d %d %d\t|\t%d %d %d %d\n",
+                process->pid,
+                process->arrivalTime,
+                process->totalCpuTime,
+                process->cpuBurst,
+                process->ioBurst,
+                process->staticPriority,
+                process->finishTime,
+                process->turnaroundTime,
+                process->ioTime,
+                process->cpuWaitTime);
+    }
+    // TODO: add summary info
 }
