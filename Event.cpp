a7a7080b2028fcbfb3ff5c8a5380d91bd806bbde
Shubham Jha <jha1shubham@gmail.com>
2021-10-29 02:05:31 -0400

    Update comparator to use PID when event with the same timestamp are generated at the same time

--- Event.cpp
+++ Event.cpp
@@ -11,6 +11,9 @@ Event::Event(Process *process, int timeStamp, ProcessState processState, Transit
 bool Event::Comparator::operator()(Event *eventA, Event *eventB) {
     // TODO: comparison code here. Ensure that sorting is stable.
     if(eventA -> timeStamp == eventB -> timeStamp) {
+        if(eventA->eventGenerationTimestamp == eventB-> eventGenerationTimestamp) {
+            return eventA -> process -> pid > eventB -> process -> pid; // Especially useful when reading the input file
+        }
         return eventA -> eventGenerationTimestamp > eventB -> eventGenerationTimestamp;
     }
     return eventA -> timeStamp > eventB -> timeStamp;
